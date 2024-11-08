59d89dcda87bfb6bbe943a455ceb0353eee3cefd
klelia <clelia.fradella@gmail.com>
2024-11-08 13:11:30 +0100

    form

--- script/script.js
+++ script/script.js
@@ -36,23 +36,79 @@ const colors = [
 //   `;
 // }
 // palette.innerHTML = template;
+function generatePalette() {
+  const container = document.querySelector(".container");
 
-const container = document.querySelector(".container");
-console.log(container);
+  /*Disegno palette di colori */
+  const elUl = document.createElement("ul");
+  elUl.className = "d-flex justify-content-between";
 
-const elUl = document.createElement("ul");
-elUl.className = "d-flex justify-content-between";
+  elUl.setAttribute("id", "palette");
 
-elUl.setAttribute("id", "palette");
+  //console.log(elUl);
+  for (let i = 0; i < colors.length; i++) {
+    const elementLi = document.createElement("li");
+    elementLi.style.backgroundColor = colors[i];
+    elementLi.addEventListener("click", function () {
+      alert(colors[i]);
+    });
+    elUl.appendChild(elementLi);
+  }
 
-//console.log(elUl);
-for (let i = 0; i < colors.length; i++) {
-  const elementLi = document.createElement("li");
-  elementLi.style.backgroundColor = colors[i];
-  elementLi.addEventListener("click", function () {
-    alert(colors[i]);
-  });
-  elUl.appendChild(elementLi);
+  container.appendChild(elUl); //prependChild appende per primo
 }
+//generatePalette();
 
-container.appendChild(elUl);
+const elInputName = document.getElementById("name");
+const elInputAge = document.getElementById("age");
+const elInputKm = document.getElementById("km");
+console.log(elInputName);
+
+const elP = document.getElementById("result");
+console.log(elP);
+
+//Con evento change sulla casella di input
+// elInputName.addEventListener("change", function (event) {
+//   console.log(elInputName.value);
+//   elP.innerText = elInputName.value;
+// });
+
+//Con evento input sulla casella di input
+// elInputName.addEventListener("input", function (event) {
+//   console.log(elInputName.value);
+//   elP.innerText = elInputName.value;
+// });
+
+// const myButton = document.querySelector("a");
+
+// myButton.addEventListener("click", function (event) {
+//   event.preventDefault();
+//   console.log(event);
+//   console.log(event.currentTarget);
+
+//   console.log(elInputName.value);
+//   elP.innerText = elInputName.value;
+// });
+
+const myform = document.getElementById("login");
+
+myform.addEventListener("submit", function (event) {
+  event.preventDefault();
+  //reset per fare altri biglietti
+  elP.innerHTML = "";
+  elP.classList.add("d-none");
+
+  // console.log(event);
+  // console.log(event.currentTarget);
+  const priceKm = 0.21;
+
+  const km = parseInt(elInputKm.value);
+  const price = priceKm * km;
+
+  elP.innerHTML = price.toFixed(2) + "€";
+  elP.classList.remove("d-none");
+  //reset per fare altri biglietti
+  elInputKm.value = "";
+  elInputAge.value = "";
+  elInputName.value = "";
+});
